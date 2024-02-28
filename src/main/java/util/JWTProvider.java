da5b47e12cf29b7af3d2a4e3d99e415a4045392b
ethank482 <>
2024-02-28 14:32:19 +0700

    add login, register, home page

---
+++ src/main/java/util/JWTProvider.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.sql.Date;
+
+public class JWTProvider {
+//    private final String JWT_SECRET = "lodaaaaaa";
+//
+//    //Thời gian có hiệu lực của chuỗi jwt
+//    private final long JWT_EXPIRATION = 604800000L;
+//
+//    // Tạo ra jwt từ thông tin user
+//    public String generateToken(CustomUserDetails userDetails) {
+//          }
+//
+//    // Lấy thông tin user từ jwt
+//    public Long getUserIdFromJWT(String token) {
+//    
+//    }
+//
+//    public boolean validateToken(String authToken) {
+//     
+//        return false;
+//    }
+}
