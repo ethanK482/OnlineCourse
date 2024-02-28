dcf0cd561504efbd7cfb54f93389bf10888d2c64
hk12 <kimhuyhuynh30@gmail.com>
2024-02-28 18:26:23 +0700

    add jwt

--- src/main/java/util/JWTProvider.java
+++ src/main/java/util/JWTProvider.java
@@ -1,24 +1,42 @@
 package util;
 
-import java.sql.Date;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JWTProvider {
-//    private final String JWT_SECRET = "lodaaaaaa";
-//
-//    //Thời gian có hiệu lực của chuỗi jwt
-//    private final long JWT_EXPIRATION = 604800000L;
-//
-//    // Tạo ra jwt từ thông tin user
-//    public String generateToken(CustomUserDetails userDetails) {
-//          }
-//
-//    // Lấy thông tin user từ jwt
-//    public Long getUserIdFromJWT(String token) {
-//    
-//    }
-//
-//    public boolean validateToken(String authToken) {
-//     
-//        return false;
-//    }
+
+    private final String JWT_SECRET = "lodaaaaaa";
+    private Algorithm algorithm = Algorithm.HMAC256(JWT_SECRET);
+    public  String generateToken(String userID, String role) {
+
+        String token = JWT.create()
+                .withIssuer("online_course")
+                .withSubject(userID)
+                .withClaim("role", role)
+                .sign(algorithm);
+        return token;
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .withIssuer("online_course") // Make sure issuer matches
+                    .build()
+                    .verify(authToken);
+
+            System.out.println("Token verified. Subject: " +  jwt.getClaims());
+        } catch (JWTDecodeException e) {
+            // Invalid token
+            System.out.println("Invalid token: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        JWTProvider jwt = new JWTProvider();
+        System.out.println(jwt.generateToken("123", "admin"));
+       jwt.validateToken(jwt.generateToken("123", "admin"));
+    }   
 }
